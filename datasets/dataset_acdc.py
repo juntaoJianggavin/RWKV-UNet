40340715efa0337b20a34689e047333d642f7abd
juntaoJianggavin <72411640+juntaoJianggavin@users.noreply.github.com>
2025-10-21 02:44:12 +0800

    Enhance image augmentation functions
    Added functions for random brightness, contrast adjustments, and cropping. Updated rotation angle range and modified random selection probabilities.

--- datasets/dataset_acdc.py
+++ datasets/dataset_acdc.py
@@ -3,7 +3,7 @@ import os
 import random
 import re
 from glob import glob
-
+from skimage import transform
 import cv2
 import h5py
 import numpy as np
@@ -92,9 +92,36 @@ def random_rot_flip(image, label):
 
 
 def random_rotate(image, label):
-    angle = np.random.randint(-20, 20)
+    angle = np.random.randint(-30, 30)
     image = ndimage.rotate(image, angle, order=0, reshape=False)
     label = ndimage.rotate(label, angle, order=0, reshape=False)
+    return image, label
+
+
+def random_brightness_contrast(image, label):
+    brightness_factor = np.random.uniform(-0.2, 0.2)
+    image = image * (1 + brightness_factor)
+    contrast_factor = np.random.uniform(0.8, 1.2)
+    image = (image - image.mean()) * contrast_factor + image.mean()
+    image = np.clip(image, 0, 1) if image.max() <= 1 else np.clip(image, 0, 255)
+    return image, label
+
+
+def random_crop(image, label):
+    x, y = image.shape
+    crop_ratio = np.random.uniform(0.8, 1.2)
+    crop_size = (int(x * crop_ratio), int(y * crop_ratio))
+    if crop_size[0] >= x:
+        crop_size = (x, crop_size[1])
+    if crop_size[1] >= y:
+        crop_size = (crop_size[0], y)
+    x_start = np.random.randint(0, x - crop_size[0] + 1)
+    y_start = np.random.randint(0, y - crop_size[1] + 1)
+    image_crop = image[x_start:x_start+crop_size[0], y_start:y_start+crop_size[1]]
+    label_crop = label[x_start:x_start+crop_size[0], y_start:y_start+crop_size[1]]
+    image = transform.resize(image_crop, (x, y), order=0, preserve_range=True)
+    label = transform.resize(label_crop, (x, y), order=0, preserve_range=True)
+    
     return image, label
 
 
@@ -104,17 +131,19 @@ class RandomGenerator(object):
 
     def __call__(self, sample):
         image, label = sample['image'], sample['label']
-        # ind = random.randrange(0, img.shape[0])
-        # image = img[ind, ...]
-        # label = lab[ind, ...]
         if random.random() > 0.5:
             image, label = random_rot_flip(image, label)
-        elif random.random() > 0.5:
+        if random.random() > 0.7:
             image, label = random_rotate(image, label)
+        if random.random() > 0.7:
+            image, label = random_brightness_contrast(image, label)
+        
+        if random.random() > 0.7:
+            image, label = random_crop(image, label)
         x, y = image.shape
         if x != self.output_size[0] or y != self.output_size[1]:
-            image = zoom(image, (self.output_size[0] / x, self.output_size[1] / y), order=0)  # the default is 0
-            label = zoom( label, (self.output_size[0] / x, self.output_size[1] / y), order=0)
+            image = zoom(image, (self.output_size[0] / x, self.output_size[1] / y), order=0)
+            label = zoom(label, (self.output_size[0] / x, self.output_size[1] / y), order=0)
 
         assert (image.shape[0] == self.output_size[0]) and (image.shape[1] == self.output_size[1])
         image = torch.from_numpy(image.astype(np.float32)).unsqueeze(0)
